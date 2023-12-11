072ed82e032cdcaedd9f7fb11eff1f2b931d70a4
Geardung <github.passis228sherdon@mail.ru>
2023-12-11 20:26:21 +0300

    ready

--- install.bat
+++ install.bat
@@ -1,5 +1,7 @@
+@echo off
 git clone https://github.com/Geardung/LethalCompatibles
 cd LethalCompatibles
 git config --global --add safe.directory .
+py -m pip install --upgrade pip
 py -m pip install -r reqs.txt
 start start.bat
--- start.bat
+++ start.bat
@@ -1,4 +1,3 @@
 @echo off
 git pull
-pause
 py script.py
