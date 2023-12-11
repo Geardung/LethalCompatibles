429fbedcfe7dbf4954cca7e6c957f4bd4e6bdb2b
Geardung <github.passis228sherdon@mail.ru>
2023-12-11 21:52:56 +0300

    delete installer from git

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-git clone https://github.com/Geardung/LethalCompatibles
-cd LethalCompatibles
-git config --global --add safe.directory .
-py -m pip install --upgrade pip
-py -m pip install -r reqs.txt
-start start.bat
