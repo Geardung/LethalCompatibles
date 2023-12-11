ea14a505952f7c58da3e9ae3989f2f698b18b911
Geardung <github.passis228sherdon@mail.ru>
2023-12-11 20:16:22 +0300

    Init commit

---
+++ packer.py
@@ -0,0 +1,92 @@
+import os, datetime, ujson, time, ftplib, shutil
+from zipfile import ZipFile
+
+
+class ModpackLoader():
+    
+    def __init__(self, ip: str, user, password):
+        
+        if not os.path.exists("./modpacks"): os.makedirs("./modpacks")
+        
+        self.version = "0.1"
+        self._server_ip: str = ip
+        self._user:str = user
+        self._password:str = password
+        
+        self._main_config = ujson.loads(open("./configs/main.json", "r").read())
+    
+    def create_n_upload_new_modpack(self):
+        
+        name = self.pack_now_modpack()
+        self.upload_modpack_on_server(name)
+    
+    def pack_now_modpack(self, name: str = str(int(datetime.datetime.now().timestamp()))):
+        
+        os.system('cls' if os.name=='nt' else 'clear')
+        
+        print("Начинаем создавать модпак: " + name)
+        
+        
+        if not os.path.exists(self._main_config["path_to_game"] + "/Lethal Company.exe"): return self._on_game_path_not_exists()
+        print("Путь к игре прописан верно")
+        if not os.path.exists(self._main_config["path_to_game"] + "/BepInEx"): return self._on_bepinex_not_exists()
+        print("Модлоадер установлен")
+        
+        def add_in_zip_dir(full_dir: str, local_dir: str, zip: ZipFile):
+            
+            zip.mkdir(local_dir)
+            
+            for file_n_dir in os.listdir(full_dir):
+                print("| " + local_dir + "/" + file_n_dir)
+                if os.path.isfile(full_dir + "/" + file_n_dir): zip.write(full_dir + "/" + file_n_dir, local_dir + "/" + file_n_dir)
+                else: add_in_zip_dir(full_dir + "/" + file_n_dir, local_dir + "/" + file_n_dir, zip)
+            
+        with ZipFile("./modpacks/" + name + ".zip", "w") as myzip:
+            print("Начинаем упаковывать модпак в ./modpacks/" + name + ".zip")
+            myzip.setpassword("Geardung".encode("utf-8"))
+            
+            for file_n_dir in os.listdir(self._main_config["path_to_game"] + "/BepInEx"):
+                print("| " + file_n_dir)
+                if os.path.isfile(self._main_config["path_to_game"] + "/BepInEx" + "/" + file_n_dir): myzip.write(self._main_config["path_to_game"] + "/BepInEx/" + file_n_dir, "BepInEx/" + file_n_dir)
+                else: add_in_zip_dir(self._main_config["path_to_game"] + "/BepInEx" + "/" + file_n_dir, "/BepInEx/" + file_n_dir, myzip)
+                time.sleep(0.35)
+                
+            print("Сохраняем сборку")
+            myzip.close()
+        
+        return name
+        
+    
+    def _on_bepinex_not_exists(): pass
+    def _on_game_path_not_exists(): pass
+    
+    
+    def upload_modpack_on_server(self, name):
+        print("Создаём соединение с сервером...")
+        with ftplib.FTP(self._server_ip) as session:
+            session.login()
+        
+            print("Подключились!\nПроверяем наличие модпака с схожим названием...")
+            if name + ".zip" in session.nlst():
+                if input("Сборка с таким названием уже существует\nПерезаписать? [Yes/No]").lower()[0] == "y": session.delete("./modpacks/" + name + ".zip")
+                else: return
+            else: print("Отлично! Модпака с таким названием нет")
+        
+            session.storbinary('STOR /home/lethal/modpacks/'+name+".zip", open('./modpacks/'+name+".zip",'rb'))
+            session.quit()
+            
+    #
+    #
+    #
+    
+    def install_modpack(self, name): 
+        from downloader import download
+        
+        download([("http://" + self._server_ip + "/modpacks/"+ name + ".zip")],
+                 dest_dir="./modpacks")
+        
+        if os.path.exists(self._main_config["path_to_game"]+"/BepInEx"): shutil.rmtree(self._main_config["path_to_game"]+"/BepInEx")
+        
+        with ZipFile("./modpacks/"+ name + ".zip", "r") as zfile:
+            
+            zfile.extractall(self._main_config["path_to_game"])
---
+++ script.py
@@ -0,0 +1,190 @@
+import os, ujson, time, datetime
+from ftplib import FTP
+config = {
+    "path_to_game": "Z:\SteamLibrary\steamapps\common\Lethal Company",
+}
+
+width = 35
+
+server_config = {
+    "label": "Санин сервер",
+    
+    "ip": "176.119.156.94",
+    "port": 21,
+    
+    "user": "lethal",
+    "password": "kiragay"
+}
+
+server_modpacks: list[str]
+
+now_server: dict[str, str]
+
+servers:list[server_config] = []
+
+
+
+
+def let_comp_path_input():
+    while True:
+        
+        print("Вам необходимо ввести полный путь до корневой папки Lethal Company")
+        a = input("\n: ")
+        if os.path.exists(a + "/Lethal Company.exe"):
+            config["path_to_game"] = a
+            return a
+        print("Путь не существует, попробуйте снова")
+
+def select_server():
+    while True:
+        
+        os.system("clear")
+
+def connect_to_server(hz: dict):
+    print("Устанавливаем соединение с " + hz["ip"])
+    
+    with FTP(hz["ip"]) as ftp:
+        
+        ftp.login()
+        ftp.cwd("modpacks")
+        all_files = ftp.nlst()
+        print(all_files, ftp.pwd())
+        server_modpacks = [x[0:-4] for x in all_files if x.endswith(".zip")]
+        print(server_modpacks)
+        ftp.close()
+        
+    return server_modpacks
+    
+def select_menu(server_modpacks):
+    a = "0"
+    
+    while True:
+        cls()
+        
+        if a.isdigit():
+            
+            if a == "1": install_modpack_menu(server_modpacks)
+            elif a == "2": create_modpack_menu()
+        
+        print("#"+"".center(width, "=")+"#")
+        print("#"+" Меню синхронайзера ".center(width, "=")+"#")
+        print("#"+"".center(width, "=")+"#")
+        print("#"+"".center(width, "-")+"#")
+        print("#"+(" Модпак сервер: " + now_server["label"]+" ").center(width, "-")+"#")
+        print("#"+(" Модпаков: "+str(len(server_modpacks))+" ").center(width, "-")+"#")
+        print("#"+"".center(width, "-")+"#")
+        print("#"+" 1. Установить модпак ".center(width, "-")+"#")
+        print("#"+" 2. Опубликовать модпак ".center(width, "-")+"#")
+        print("#"+"".center(width, "-")+"#")
+        print("#"+"".center(width, "=")+"#")
+        
+        a = input("\nВыберите действие: ")
+
+def create_modpack_menu():
+    
+    from packer import ModpackLoader
+    
+    instance = ModpackLoader(now_server["ip"],
+                             now_server["user"],
+                             now_server["password"])
+    
+    instance.create_n_upload_new_modpack()
+
+def install_modpack_menu(server_modpacks):
+    a = "0"
+    
+    if len(server_modpacks) == 0: 
+        print("\nОтсутствуют модпаки на текущем сервере!\n")
+        time.sleep(1.5)
+        return select_menu(server_modpacks)
+    
+    a = ""
+    
+    while True:
+        cls()
+        
+        if a.isdigit():
+            from packer import ModpackLoader
+            if a == "0": select_menu(server_modpacks)
+            
+            if a in server_modpacks: ModpackLoader(now_server["ip"],
+                             now_server["user"],
+                             now_server["password"]).install_modpack(a)
+            
+            elif int(a)-1 in range(len(server_modpacks)): ModpackLoader(now_server["ip"],
+                             now_server["user"],
+                             now_server["password"]).install_modpack(server_modpacks[int(a)-1])
+        
+        print("#"+"".center(width, "=")+"#")
+        print("#"+" Выбор модпака ".center(width, "=")+"#")
+        print("#"+"".center(width, "=")+"#")
+        print("#"+"".center(width, "-")+"#")
+        print("#"+(" Модпак сервер: " + now_server["label"]+" ").center(width, "-")+"#")
+        print("#"+(" Модпаков: "+str(len(server_modpacks))+" ").center(width, "-")+"#")
+        print("#"+"".center(width, "-")+"#")
+        for ind, modpack in enumerate(server_modpacks):
+            print("#"+ (" "+str(ind+1)+". " + modpack +" ").center(width, "-")+"#")
+        print("#"+"".center(width, "-")+"#")
+        print("#"+"".center(width, "=")+"#")
+        
+        a = input("Введите модпак для скачки (\"0\" чтобы назад): ")
+
+
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
+if __name__ == "__main__":
+    cls()
+    
+    if not os.path.exists("./configs"): os.mkdir("./configs")
+    
+    print("Проверка главного конфига")
+    
+    if not os.path.exists("./configs/main.json"): 
+        
+        print("- Отсутствует. Инициализируем новый")
+        
+        with open("./configs/main.json", "w", encoding="utf-8") as f:
+            let_comp_path_input()
+            f.write(ujson.dumps(config, ensure_ascii=True, encode_html_chars=True))
+    else:
+        print("- Присутствует")
+            
+    
+    if not os.path.exists("./configs/176.119.156.94.json"):
+        with open("./configs/176.119.156.94.json", "w", encoding="utf-8") as f:
+            f.write(ujson.dumps(server_config))
+    
+    ###
+    ###
+    ###
+    
+    for file in os.listdir("./configs"):
+        
+        if file == "main.json": continue
+        
+        if file.count(".") == 4 and file.endswith(".json"): 
+            
+            with open("./configs/" + file, "r", encoding="utf-8") as f:
+                
+                f:dict = ujson.loads(f.read())
+                
+                servers.append({
+                    "label": f.get("label", "Default label"),
+                    "ip": f.get("ip", "176.119.156.94"),
+                    "port": f.get("port", 21),
+                    
+                    "user": f.get("user", "lethal"),
+                    "password": f.get("kiragay", "Default label"),
+                })
+
+    if len(servers) == 0: print("Конфигов серверов нема, попробуйте удалить папку configs, чтобы синициализировался основной сервер")
+    elif len(servers) == 1:
+        print("Так-как сервер всего один, то подключаемся к нему...")
+        now_server = servers[0]
+        
+    server_modpacks = connect_to_server(now_server)
+    select_menu(server_modpacks)
+        
+    
